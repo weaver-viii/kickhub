53e91d994d7c4a6e63845fe1e9d6793936603e13
Bastien Guerry <bzg@altern.org>
2013-09-28 12:04:08 +0200

    Add basic friend interactive form for login.
    Only one user is defined so far.

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :immutant {:context-path "/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]
-                 [com.draines/postal "1.10.3"]])
+  :dependencies
+  [[org.clojure/clojure "1.5.1"]
+   [com.cemerick/friend "0.1.5"]
+   [compojure "1.1.5"]
+   [com.draines/postal "1.10.3"]
+   [lib-noir "0.6.6"]])
--- src/kickhub/core.clj
+++ src/kickhub/core.clj
@@ -1,10 +1,15 @@
 (ns kickhub.core
   (:require
+   [noir.util.middleware :as middleware]
    [compojure.core :as compojure :refer (GET POST defroutes)]
    (compojure [handler :as handler]
               [route :as route])
    [hiccup.page :as h]
-   [postal.core :as postal]))
+   [postal.core :as postal]
+   [cemerick.friend :as friend]
+   (cemerick.friend [workflows :as workflows]
+                    [credentials :as creds])
+   [cemerick.friend.credentials :refer (hash-bcrypt)]))
 
 (defn- index []
   (h/html5
@@ -23,10 +28,48 @@
     :body "Hopefully this message will be more interesting soon."})
   "Email sent.")
 
+(defn- login
+  "Display the login page."
+  [req]
+  (h/html5
+   (h/include-css "/css/kickhub.css")
+   [:body
+    [:h1 "KickHub"]
+    [:h2 "Login"]
+    [:form {:method "POST" :action "login"}
+     [:div "Email: " [:input {:type "text" :name "username"}]]
+     [:div "Password: " [:input {:type "password" :name "password"}]]
+     [:div [:input {:type "submit" :class "button" :value "Login"}]]]]))
+
+(defn- load-users [username]
+  {:username username
+   :password (hash-bcrypt "password")
+   :roles #{::user}})
+
+(defn wrap-friend [handler]
+  "Wrap friend authentication around handler."
+  (friend/authenticate
+   handler
+   {:allow-anon? true
+    :workflows [(workflows/interactive-form
+                 ;; :allow-anon? true
+                 ;; :login-uri "/login"
+                 ;; :default-landing-uri "/login"
+                 :credential-fn
+                 (partial creds/bcrypt-credential-fn load-users))]}))
+
 (defroutes app-routes
   (GET "/" [] (index))
   (GET "/sendmail/:user" [user] (send-email user))
+  (GET "/login" req (login req))
+  (GET "/check" req
+       (if-let [identity (friend/identity req)]
+         (apply str "Logged in, with these roles: "
+                (-> identity friend/current-authentication :roles))
+         "You are an anonymous user."))
   (route/resources "/")
   (route/not-found "Sorry, page not found."))
 
-(def ring-handler app-routes)
+(def ring-handler
+  (middleware/app-handler
+   [(wrap-friend app-routes)]))
