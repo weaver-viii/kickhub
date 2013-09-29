2a412e806a5acae8400544634e845591a74a1d00
Bastien Guerry <bzg@altern.org>
2013-09-29 10:27:11 +0200

    Bump versions when possible.
    Note: bumping from lib-noir 0.6.8 to lib-noir 0.7.0 throws an error.

--- project.clj
+++ project.clj
@@ -9,8 +9,9 @@
    [com.cemerick/friend "0.1.5"]
    [friend-oauth2 "0.0.4"]
    [compojure "1.1.5"]
-   [com.draines/postal "1.10.3"]
-   [lib-noir "0.6.6"]
-   [ring-server "0.2.8"]
+   [com.draines/postal "1.11.0"]
+   [lib-noir "0.6.8"]
+   [ring-server "0.3.0"]
    [digest "1.4.3"]
-   [com.taoensso/carmine "2.0.0"]])
+   [com.taoensso/carmine "2.2.1"]
+   [enlive "1.1.4"]])
