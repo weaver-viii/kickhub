55f542f79651111b4f0966fdc52a6f30474478ba
Bastien Guerry <bzg@altern.org>
2013-11-27 10:27:14 +0100

    project.clj: Add :nrepl-port

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://kickhub.clojurecup.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :immutant {:context-path "/"}
+  :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [com.cemerick/friend "0.1.5"]
