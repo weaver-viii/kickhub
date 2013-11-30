9f55729b78f0b28c208ba7f4769a1865ffcf2b5e
Bastien Guerry <bzg@altern.org>
2013-11-30 09:17:10 +0100

    project.clj: use lein ancient to bump versions
    Also add [com.cemerick/friend "0.2.0" :exclusions [org.clojure/core.cache]]
    to circumvent dependency problem.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kickhub "0.1.0-SNAPSHOT"
+(defproject kickhub "0.1.1-SNAPSHOT"
   :description "KickHub: boost donations to free software/content"
   :url "http://kickhub.clojurecup.com"
   :license {:name "Eclipse Public License"
@@ -6,13 +6,13 @@
   :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [com.cemerick/friend "0.2.0"]
-   [friend-oauth2 "0.0.4"]
-   [compojure "1.1.5"]
-   [com.draines/postal "1.11.0"]
-   [lib-noir "0.6.8"]
+   [com.cemerick/friend "0.2.0" :exclusions [org.clojure/core.cache]]
+   [friend-oauth2 "0.1.1"]
+   [compojure "1.1.6"]
+   [com.draines/postal "1.11.1"]
+   [lib-noir "0.7.6"]
    [clojurewerkz/scrypt "1.0.0"]
-   [ring-server "0.3.0"]
+   [ring-server "0.3.1"]
    [digest "1.4.3"]
-   [com.taoensso/carmine "2.2.1"]
+   [com.taoensso/carmine "2.4.0"]
    [enlive "1.1.4"]])
