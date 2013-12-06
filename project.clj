78f3ee8303c00e6cc05508fe7718dfc4fd4375de
Bastien Guerry <bzg@altern.org>
2013-12-06 16:26:09 +0100

    Use the GNU AGPL license.

--- project.clj
+++ project.clj
@@ -1,18 +1,21 @@
 (defproject kickhub "0.1.1-SNAPSHOT"
-  :description "KickHub: boost donations to free software/content"
-  :url "http://kickhub.clojurecup.com"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "KickHub: boost donations to free softwares"
+  :url "http://kickhub.com"
+  :license {:name "GNU Affero General Public License version 3"
+            :url "http://www.gnu.org/licenses/agpl.txt"
+            :repo "https://github.com/bzg/kickhub"}
   :immutant {:context-path "/" :nrepl-port 4005}
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [com.cemerick/friend "0.2.0" :exclusions [org.clojure/core.cache]]
+   [org.clojure/data.json "0.2.3"]
    [friend-oauth2 "0.1.1"]
+   [cheshire "5.2.0"]
    [compojure "1.1.6"]
    [com.draines/postal "1.11.1"]
    [lib-noir "0.7.6"]
    [clojurewerkz/scrypt "1.0.0"]
-   [ring-server "0.3.1"]
+   [ring-server "0.3.1" :exclusions [ring]]
    [digest "1.4.3"]
-   [com.taoensso/carmine "2.4.0"]
-   [enlive "1.1.4"]])
+   [com.taoensso/carmine "2.4.1"]
+   [enlive "1.1.5"]])
