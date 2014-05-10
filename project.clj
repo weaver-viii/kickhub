a083943c6997b9c5669fbbefbcce9401b7780d50
Bastien Guerry <bzg@altern.org>
2014-05-10 22:01:49 +0200

    project.clj: Bump versions.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :repo "https://github.com/bzg/kickhub"}
   :codox {:sources ["src/kickhub"]}
   :dependencies
-  [[org.clojure/clojure "1.5.1"]
+  [[org.clojure/clojure "1.6.0"]
    [com.cemerick/friend "0.2.0" :exclusions [org.apache.httpcomponents/httpclient org.clojure/core.cache]]
    [org.clojure/data.json "0.2.4"]
    [friend-oauth2 "0.1.1"]
@@ -14,12 +14,12 @@
    [prismatic/schema "0.2.2"]
    [simple-time "0.1.1"]
    [cheshire "5.3.1"]
-   [compojure "1.1.6"]
+   [compojure "1.1.7"]
    [com.draines/postal "1.11.1"]
    [lib-noir "0.8.2"]
    [clojurewerkz/scrypt "1.1.0"]
    [http-kit "2.1.18"]
-   [clj-rss "0.1.6"]
+   [clj-rss "0.1.7"]
    [clj-http "0.9.1"]
    [digest "1.4.4"]
    [com.taoensso/carmine "2.6.2"]
